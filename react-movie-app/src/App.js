75c5c5038bf5759e1dc8b94803f955b6d272d54f
jason-moonKor <jaesangv@naver.com>
2022-01-29 00:19:07 +0900

    1/29 update

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -1,13 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Button from "./Button";
+import styles from "./App.module.css";
+import {useState} from "react";
 
 function App() {
+	const [count, setValue] = useState(0);
+	const onClick = () => setValue((a) => a + 1);
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<h1>WELCOME TO WORLD</h1>
-			</header>
+		<div>
+			<h1 className={styles.title}>WELCOME TO WORLD</h1>
+			<h1>{count}</h1>
+			<Button text={"버튼입니다"} />
+			<button onClick={onClick}>클릭하십시오</button>
 		</div>
 	);
 }
